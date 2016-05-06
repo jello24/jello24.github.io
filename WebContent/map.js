687d226ffb3352db21418e310c66f97a23e82b82
Jan Louis Evangelista <jlevangelista@alumni.ubc.ca>
2016-05-05 18:22:09 -0700

    Create map.js, style.css

---
+++ WebContent/map.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+var number
+
+document.write("This is a JavaScript script")
+
+for(number = 0; number < 10; number++)
+	{
+		document.write(number)
+		document.write("<br>")
+	}
+
+document.write("Finished counting")
